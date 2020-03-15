3c842636c8a4f19076a835c2c0de741881c21a68
charckle <andrej.zubin@email.com>
2020-03-15 18:08:57 +0100

    setting up the forms

--- app/main_page_module/forms.py
+++ app/main_page_module/forms.py
@@ -2,7 +2,7 @@
 from flask_wtf import FlaskForm # , RecaptchaField
 
 # Import Form elements such as TextField and BooleanField (optional)
-from wtforms import StringField, PasswordField, SubmitField, validators # BooleanField
+from wtforms import IntegerField, StringField, PasswordField, SubmitField, TextAreaField, validators # BooleanField
 
 # Import Form validators
 from wtforms.validators import Email, EqualTo, ValidationError
@@ -50,6 +50,55 @@ class RegisterForm(FlaskForm):
                 raise ValidationError('Please use a different email address.')     
         
         else:  
-            raise ValidationError('Please use a valid email address.')          
+            raise ValidationError('Please use a valid email address.')
         
+class LocationForm(FlaskForm):
+    name          = StringField('Location Name', [validators.InputRequired(message='We need the name of the location.')])
+    description_s = StringField('Short Description', [validators.InputRequired(message='We need a short description.')])
+    description_l = TextAreaField('long Description', [validators.InputRequired(message='Write the long description.'),
+                                               validators.Length(max=1300)])    
+    
+    categories = [(0, 'Uncategorised'), (1, 'History'), (2, 'Nature'), (3, 'Fun'), (4, 'Castle'), (5, 'Cave'), (6, 'Waterfall'), (7, 'Hilltop/Mountain')]
+    category   = SelectField('category', choices=categories, [validators.InputRequired(message='Select a Category.')], coerce=int)
+    
+    photo_main = FileField(u'Main photo', [validators.regexp(u'^[^/\\]\.(jpg|JPG|jpeg|JPEG|png|PNG)$'),
+                                           validators.InputRequired(message='Select a Category.')])
+    
+    ratings  = [(0, 'If you have time, go see it once'), (1, 'Nothing special, but nice to see'), (2, 'Worth seeing if you can spare the time'), (3, 'Check it out, you wont regret it'), (4, 'Really good, top and see it'), (5, 'A must every time!')]
+    rating   = SelectField('Rating', choices=ratings, [validators.InputRequired(message='Select a Rate.')], coerce=int)
+    
+    tts      = IntegerField('tts', [validators.InputRequired(message='We need a username for your account.')])
+    lat_l    = DecimalField('lat_l', [validators.InputRequired(message='We need a username for your account.')], places=7)
+    lon_l    = DecimalField('lon_l', [validators.InputRequired(message='We need a username for your account.')], places=7)
+    lat_l_s  = DecimalField('lat_l_s', [validators.InputRequired(message='We need a username for your account.')], places=7)
+    lon_l_s  = DecimalField('lon_l_s', [validators.InputRequired(message='We need a username for your account.')], places=7)
+    
+    mtlds = [(0, 'PP0 - Easy, do not bother to mention'), (1, 'PP1 - Peacefull walk'), (2, 'PP2 - Can have some climbing protections'), (3, 'PP3 - Climbing protections, but not exposed, phisical strenght needed'), (4, 'PP4 - Vertical, exposed route'), (5, 'PP5 - Basicaly rock climbing with steel cables'), (6, 'PP6 - Like 5 but harder and more vertica')]
+    mtld     = SelectField('Max To Location Difficulty', choices=mtlds, [validators.InputRequired(message='Select a Difficulty.')], coerce=int)
+    
+    webpage  = StringField('webpage', [validators.InputRequired(message='We need a username for your account.')])
+    telephone  = StringField('telephone', [validators.InputRequired(message='We need a username for your account.')])
+    email     = StringField('email', [validators.InputRequired(message='We need a username for your account.')])
+    
+    timetable  = StringField('timetable', [validators.InputRequired(message='We need a username for your account.')])
+    
+    fees = [(0, 'No'), (1, 'Depends'), (2, 'Yes')]
+    fee  = RadioField('Season Dependand', choices=fees, [validators.InputRequired(message='Select if the location has fees.')], coerce=int)   
+    
+    childs = [(0, 'No'), (1, 'Depends'), (2, 'Yes')]
+    child  = RadioField('Suitable for children', choices=childs, [validators.InputRequired(message='Select if the location is suitable for children.')], coerce=int)   
+    
+    seasons = [(0, 'No'), (1, 'Yes')]
+    season  = RadioField('Season Dependand', choices=seasons, [validators.InputRequired(message='Select if the location is season dependant.')], coerce=int)    
+    
+    submit = SubmitField('Submit')
+    
+    def validate_email(self, email):
+        
+        regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+        
+        #check if it is a real email
+        if not(re.search(regex,email.data)):  
+            raise ValidationError('Please use a valid email address.')               
+      
         
--- app/main_page_module/models.py
+++ app/main_page_module/models.py
@@ -59,5 +59,86 @@ class Location(Base):
 
     __tablename__ = 'users_table'
     
-    # User Name
-    name    = db.Column(db.String(128),  nullable=False)    
+    # Creator ID
+    creator_id  = db.Column(db.Integer(7),  nullable=False)    
+    # Name
+    name  = db.Column(db.String(128),  nullable=False)
+    # short description
+    description_s = db.Column(db.String(128),  nullable=False)
+    # long description
+    description_l = db.Column(db.String(1300),  nullable=False)  
+    # Main Photo
+    photo_main  = db.Column(db.String(128),  nullable=False)    
+    # Category
+    category = db.Column(db.Integer(3),  nullable=False)
+    # Rating
+    rating = db.Column(db.Integer(2),  nullable=False)
+    # Time To Spend
+    tts = db.Column(db.Integer(4),  nullable=False)
+    # latitude of location
+    lat_l = db.Column(db.Long(20),  nullable=False)
+    # longitude of location
+    lon_l = db.Column(db.Long(20),  nullable=False)
+    # latitude of start
+    lat_l_s = db.Column(db.Long(20),  nullable=False)
+    # longitude of start
+    lon_l_s = db.Column(db.Long(20),  nullable=False)
+    # Maximum to location difficulty
+    mtld = db.Column(db.Integer(2),  nullable=False)
+    
+    # webpage
+    webpage  = db.Column(db.String(128),  nullable=False)    
+    # telephone
+    telephone  = db.Column(db.String(128),  nullable=False)
+    # email
+    email  = db.Column(db.String(128),  nullable=False)
+    
+    # timetable
+    timetable  = db.Column(db.String(128),  nullable=False)
+    # fee
+    fee = db.Column(db.Integer(1),  nullable=False)
+    # children suitable
+    child = db.Column(db.Integer(1),  nullable=False)
+    # season dependant
+    season = db.Column(db.Integer(1),  nullable=False)
+    
+    # verified - team checked out the location
+    verified = db.Column(db.Integer(1),  nullable=False)
+    # displayed - is the location displayed in the webpage
+    displayed = db.Column(db.Integer(1),  nullable=False)
+    # suggestion
+    suggestion = db.Column(db.Integer(1),  nullable=False)
+    
+    # New instance instantiation procedure
+    def __init__(self, creator_id, name, description_s, description_l, category, rating, tts, lat_l, lon_l, lat_l_s, lon_l_s, mtld, webpage, telephone, email, timetable, fee, child, season):
+
+        self.creator_id    = creator_id
+        self.name          = name
+        self.description_s = description_s
+        self.description_l = description_l
+        self.category      = category
+        self.photo_main    = photo_main
+                
+        self.rating  = rating
+        self.tts     = tts
+        self.lat_l   = lat_l
+        self.lon_l   = lon_l  
+        self.lat_l_s = lat_l_s
+        self.lon_l_s = lon_l_s
+        self.mtld    = mtld
+        
+        self.webpage  = webpage
+        self.telephone = telephone
+        self.email = email
+        
+        self.timetable = timetable   
+        self.fee       = fee
+        self.child     = child
+        self.season    = season        
+        
+        self.verified = 0
+        self.displayed = 0
+        self.suggestion = 1
+
+    def __repr__(self):
+        return '<Location %r>' % (self.name)    
